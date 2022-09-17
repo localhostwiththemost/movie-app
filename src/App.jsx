4892d2708979ec6248253a2cd4fd3f7b402cc948
localhostwiththemost <jpowelldev@gmail.com>
2022-09-17 16:06:37 -0500

    Added slider component(need to convert js code to jsx syntax)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import Carousel from "./components/Carousel";
 import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Carousel />
     </>
   );
 }
